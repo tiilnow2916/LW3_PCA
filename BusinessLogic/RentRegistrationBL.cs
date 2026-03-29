ca8e14b4affe0328662808879134248f2760ef5d
German <german.fedotov03@mail.ru>
2026-03-29 20:11:17 +0300

    hope its last commit before forming

--- BusinessLogic/RentRegistrationBL.cs
+++ BusinessLogic/RentRegistrationBL.cs
@@ -5,10 +5,33 @@ using System.Text;
 using System.Threading.Tasks;
 using DataService;
 using Entities;
+using Services;
 namespace BusinessLogic
 {
     public class RentRegistrationBL
     {
+        private readonly IRegistrationDAO registrationsDAO;
+        public RentRegistrationBL ()
+        {
+            registrationsDAO = new RentRegistrationService ();
+        }
+        public void Add(RentRegistration registration)
+        {
+            if (registration == null)
+            {
+                throw new ArgumentNullException(nameof(registration), "Registration cannot be null");
+            }
+
+            registrationsDAO.Add(registration);
+        }
+        public RentRegistration? GetByID(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentException("ID number must be positive");
+            }
+            return registrationsDAO.Get(id);
+        }
 
     }
 }
